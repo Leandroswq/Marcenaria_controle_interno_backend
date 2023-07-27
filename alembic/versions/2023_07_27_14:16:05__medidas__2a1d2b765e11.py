17801d8c39eff316932ebb31c0d6a10dc9c6b1ef
Leandroswq <leandrobonfim15@hotmail.com>
2023-07-27 20:10:26 -0300

    feat: Migration medidas popula o banco com dados de teste

--- alembic/versions/2023_07_27_14:16:05__medidas__2a1d2b765e11.py
+++ alembic/versions/2023_07_27_14:16:05__medidas__2a1d2b765e11.py
@@ -8,6 +8,9 @@ Create Date: 2023-07-27 14:16:05.804039
 from alembic import op
 import sqlalchemy as sa
 
+import os
+
+create_seed = True if os.getenv("ALEMBIC_CREATE_SEEDS") == "True" else False
 
 # revision identifiers, used by Alembic.
 revision = "2a1d2b765e11"
@@ -17,11 +20,23 @@ depends_on = None
 
 
 def upgrade() -> None:
-    op.create_table(
+    tabela = op.create_table(
         "medidas",
         sa.Column("id", sa.Integer, primary_key=True, autoincrement=True),
         sa.Column("medida", sa.String(50), nullable=False, unique=True),
     )
+
+    if create_seed:
+        medidas = [
+            {"medida": "metro"},
+            {"medida": "unidade"},
+            {"medida": "litro"},
+        ]
+        try:
+            op.bulk_insert(tabela, medidas)
+        except Exception as e:
+            op.drop_table("medidas")
+            raise e
 
 
 def downgrade() -> None:

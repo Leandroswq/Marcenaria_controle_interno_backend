3f6c33bffd7162459e8bdb51becd3ea1c264173b
Leandroswq <leandrobonfim15@hotmail.com>
2023-07-27 20:08:38 -0300

    feat: Migration funcionarios popula o banco com dados de teste

--- alembic/versions/2023_07_27_14:08:07__funcionarios__2d700beed168.py
+++ alembic/versions/2023_07_27_14:08:07__funcionarios__2d700beed168.py
@@ -8,6 +8,9 @@ Create Date: 2023-07-27 14:08:07.269639
 from alembic import op
 import sqlalchemy as sa
 
+import os
+
+create_seed = True if os.getenv("ALEMBIC_CREATE_SEEDS") == "True" else False
 
 # revision identifiers, used by Alembic.
 revision = "2d700beed168"
@@ -17,7 +20,7 @@ depends_on = None
 
 
 def upgrade() -> None:
-    op.create_table(
+    tabela = op.create_table(
         "funcionarios",
         sa.Column("id", sa.Integer, primary_key=True, autoincrement=True),
         sa.Column("nome", sa.String(50), nullable=False),
@@ -28,6 +31,33 @@ def upgrade() -> None:
             "LENGTH(telefone) = 11", name="ck_funcionarios_telefone_length"
         ),
     )
+
+    if create_seed:
+        funcionarios = [
+            {
+                "nome": "João",
+                "sobre_Nome": "Silva",
+                "funcao": "Analista",
+                "telefone": "1122334455",
+            },
+            {
+                "nome": "Maria",
+                "sobre_Nome": "Santos",
+                "funcao": "Gerente",
+                "telefone": "5566778899",
+            },
+            {
+                "nome": "Pedro",
+                "sobre_Nome": "Ferreira",
+                "funcao": "Desenvolvedor",
+                "telefone": "9988776655",
+            },
+        ]
+        try:
+            op.bulk_insert(tabela, funcionarios)
+        except Exception as e:
+            op.drop_table("funcionarios")
+            raise e
 
 
 def downgrade() -> None:

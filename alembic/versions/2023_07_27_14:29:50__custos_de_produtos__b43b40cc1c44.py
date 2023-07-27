3a3819d7b6ea02645821951ca542cb2d9f0082ee
Leandroswq <leandrobonfim15@hotmail.com>
2023-07-27 20:10:45 -0300

    feat: Migration custos de produtos popula o banco com dados de teste

--- alembic/versions/2023_07_27_14:29:50__custos_de_produtos__b43b40cc1c44.py
+++ alembic/versions/2023_07_27_14:29:50__custos_de_produtos__b43b40cc1c44.py
@@ -8,6 +8,9 @@ Create Date: 2023-07-27 14:29:50.644343
 from alembic import op
 import sqlalchemy as sa
 
+import os
+
+create_seed = True if os.getenv("ALEMBIC_CREATE_SEEDS") == "True" else False
 
 # revision identifiers, used by Alembic.
 revision = "b43b40cc1c44"
@@ -17,7 +20,7 @@ depends_on = None
 
 
 def upgrade() -> None:
-    op.create_table(
+    tabela = op.create_table(
         "custos_de_produto",
         sa.Column("id", sa.Integer, primary_key=True, autoincrement=True),
         sa.Column(
@@ -36,6 +39,63 @@ def upgrade() -> None:
             nullable=False,
         ),
     )
+
+    if create_seed:
+        custos = [
+            {
+                "produto": 1,
+                "valor": 100.50,
+                "valor_unitario": 50.25,
+                "medida": 1,
+                "quantidade": 10,
+                "ordem_de_pedido_id": 1,
+            },
+            {
+                "produto": 2,
+                "valor": 75.25,
+                "valor_unitario": 25.08,
+                "medida": 2,
+                "quantidade": 5,
+                "ordem_de_pedido_id": 1,
+            },
+            {
+                "produto": 1,
+                "valor": 300.00,
+                "valor_unitario": 150.00,
+                "medida": 3,
+                "quantidade": 2,
+                "ordem_de_pedido_id": 2,
+            },
+            {
+                "produto": 2,
+                "valor": 45.80,
+                "valor_unitario": 22.90,
+                "medida": 1,
+                "quantidade": 8,
+                "ordem_de_pedido_id": 2,
+            },
+            {
+                "produto": 3,
+                "valor": 180.00,
+                "valor_unitario": 60.00,
+                "medida": 2,
+                "quantidade": 3,
+                "ordem_de_pedido_id": 3,
+            },
+            {
+                "produto": 3,
+                "valor": 120.75,
+                "valor_unitario": 40.25,
+                "medida": 3,
+                "quantidade": 4,
+                "ordem_de_pedido_id": 3,
+            },
+        ]
+        try:
+            op.bulk_insert(tabela, custos)
+        except Exception as e:
+            op.drop_table("custos_de_produto")
+            raise e
 
 
 def downgrade() -> None:

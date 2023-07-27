c74f9101ae002f507c1e7433000a892e0098a4fa
Leandroswq <leandrobonfim15@hotmail.com>
2023-07-27 20:09:45 -0300

    feat: Migrationodens de pedido popula o banco com dados de teste

--- alembic/versions/2023_07_27_14:09:25__ordens_de_pedido__722a7ae04d39.py
+++ alembic/versions/2023_07_27_14:09:25__ordens_de_pedido__722a7ae04d39.py
@@ -7,7 +7,11 @@ Create Date: 2023-07-27 14:09:25.336193
 """
 from alembic import op
 import sqlalchemy as sa
+from datetime import datetime
 
+import os
+
+create_seed = True if os.getenv("ALEMBIC_CREATE_SEEDS") == "True" else False
 
 # revision identifiers, used by Alembic.
 revision = "722a7ae04d39"
@@ -17,7 +21,7 @@ depends_on = None
 
 
 def upgrade() -> None:
-    op.create_table(
+    tabela = op.create_table(
         "ordens_de_pedido",
         sa.Column("id", sa.Integer, primary_key=True, autoincrement=True),
         sa.Column(
@@ -31,6 +35,36 @@ def upgrade() -> None:
         sa.Column("Data prevista", sa.DateTime, nullable=False),
         sa.Column("Termino", sa.DateTime, nullable=True),
     )
+
+    if create_seed:
+        ordens_de_pedido = [
+            {
+                "cliente_id": 1,
+                "total": 1500.00,
+                "inicio": datetime(2023, 7, 27, 10, 0, 0),
+                "data_prevista": datetime(2023, 8, 10, 18, 0, 0),
+                "termino": None,
+            },
+            {
+                "cliente_id": 2,
+                "total": 2000.50,
+                "inicio": datetime(2023, 7, 28, 9, 30, 0),
+                "data_prevista": datetime(2023, 8, 15, 16, 30, 0),
+                "termino": None,
+            },
+            {
+                "cliente_id": 3,
+                "total": 3000.75,
+                "inicio": datetime(2023, 7, 30, 11, 15, 0),
+                "data_prevista": datetime(2023, 8, 12, 20, 45, 0),
+                "termino": None,
+            },
+        ]
+        try:
+            op.bulk_insert(tabela, ordens_de_pedido)
+        except Exception as e:
+            op.drop_table("ordens_de_pedido")
+            raise e
 
 
 def downgrade() -> None:

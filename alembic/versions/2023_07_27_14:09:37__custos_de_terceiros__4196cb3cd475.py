51302e1f6b4255ade12e5c761d0a9d62733ef1c0
Leandroswq <leandrobonfim15@hotmail.com>
2023-07-27 20:10:09 -0300

    feat: Migration custos de terceiros popula o banco com dados de teste

--- alembic/versions/2023_07_27_14:09:37__custos_de_terceiros__4196cb3cd475.py
+++ alembic/versions/2023_07_27_14:09:37__custos_de_terceiros__4196cb3cd475.py
@@ -8,6 +8,9 @@ Create Date: 2023-07-27 14:09:37.491108
 from alembic import op
 import sqlalchemy as sa
 
+import os
+
+create_seed = True if os.getenv("ALEMBIC_CREATE_SEEDS") == "True" else False
 
 # revision identifiers, used by Alembic.
 revision = "4196cb3cd475"
@@ -17,7 +20,7 @@ depends_on = None
 
 
 def upgrade() -> None:
-    op.create_table(
+    tabela = op.create_table(
         "custos_de_terceiros",
         sa.Column("id", sa.Integer, primary_key=True, autoincrement=True),
         sa.Column("descricao", sa.String(255), nullable=False),
@@ -42,6 +45,88 @@ def upgrade() -> None:
             nullable=False,
         ),
     )
+
+    if create_seed:
+        custos_de_terceiros = [
+            {
+                "descricao": "Custo 1",
+                "valor": 100.50,
+                "parceiro": 1,
+                "reponsavel_pelo_contato": 1,
+                "quantidade": 3,
+                "ordem_de_pedido_id": 1,
+            },
+            {
+                "descricao": "Custo 2",
+                "valor": 50.20,
+                "parceiro": 2,
+                "reponsavel_pelo_contato": 2,
+                "quantidade": 2,
+                "ordem_de_pedido_id": 2,
+            },
+            {
+                "descricao": "Custo 3",
+                "valor": 75.30,
+                "parceiro": 3,
+                "reponsavel_pelo_contato": 3,
+                "quantidade": 1,
+                "ordem_de_pedido_id": 3,
+            },
+            {
+                "descricao": "Custo 4",
+                "valor": 80.40,
+                "parceiro": 1,
+                "reponsavel_pelo_contato": 2,
+                "quantidade": 4,
+                "ordem_de_pedido_id": 1,
+            },
+            {
+                "descricao": "Custo 5",
+                "valor": 120.00,
+                "parceiro": 2,
+                "reponsavel_pelo_contato": 3,
+                "quantidade": 2,
+                "ordem_de_pedido_id": 2,
+            },
+            {
+                "descricao": "Custo 6",
+                "valor": 65.80,
+                "parceiro": 3,
+                "reponsavel_pelo_contato": 1,
+                "quantidade": 3,
+                "ordem_de_pedido_id": 3,
+            },
+            {
+                "descricao": "Custo 7",
+                "valor": 90.10,
+                "parceiro": 1,
+                "reponsavel_pelo_contato": 2,
+                "quantidade": 1,
+                "ordem_de_pedido_id": 1,
+            },
+            {
+                "descricao": "Custo 8",
+                "valor": 55.00,
+                "parceiro": 2,
+                "reponsavel_pelo_contato": 3,
+                "quantidade": 5,
+                "ordem_de_pedido_id": 2,
+            },
+            {
+                "descricao": "Custo 9",
+                "valor": 70.25,
+                "parceiro": 3,
+                "reponsavel_pelo_contato": 1,
+                "quantidade": 2,
+                "ordem_de_pedido_id": 3,
+            },
+        ]
+
+        try:
+            op.bulk_insert(tabela, custos_de_terceiros)
+        except Exception as e:
+            op.drop_table("custos_de_terceiros")
+            raise e
 
 
 def downgrade() -> None:

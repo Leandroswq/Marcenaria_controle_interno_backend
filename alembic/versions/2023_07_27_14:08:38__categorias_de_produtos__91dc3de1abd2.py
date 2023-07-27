c997af7d9c49a257a466d9a74fb298ed5ca09660
Leandroswq <leandrobonfim15@hotmail.com>
2023-07-27 20:09:02 -0300

    feat: Migration categorias de produtos popula o banco com dados de teste

--- alembic/versions/2023_07_27_14:08:38__categorias_de_produtos__91dc3de1abd2.py
+++ alembic/versions/2023_07_27_14:08:38__categorias_de_produtos__91dc3de1abd2.py
@@ -8,6 +8,9 @@ Create Date: 2023-07-27 14:08:38.951160
 from alembic import op
 import sqlalchemy as sa
 
+import os
+
+create_seed = True if os.getenv("ALEMBIC_CREATE_SEEDS") == "True" else False
 
 # revision identifiers, used by Alembic.
 revision = "91dc3de1abd2"
@@ -17,11 +20,23 @@ depends_on = None
 
 
 def upgrade() -> None:
-    op.create_table(
+    tabela = op.create_table(
         "categorias_de_produto",
         sa.Column("id", sa.Integer, primary_key=True, autoincrement=True),
         sa.Column("categoria", sa.String(50), nullable=False, unique=True),
     )
+
+    if create_seed:
+        categorias = [
+            {"categoria": "madeira"},
+            {"categoria": "ferramenta"},
+            {"categoria": "broca"},
+        ]
+        try:
+            op.bulk_insert(tabela, categorias)
+        except Exception as e:
+            op.drop_table("categorias_de_produto")
+            raise e
 
 
 def downgrade() -> None:

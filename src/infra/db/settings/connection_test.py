63f9e8b6b7c846be31f2fc85ecf4e9deb8f1ca8f
Leandroswq <leandrobonfim15@hotmail.com>
2023-07-28 16:39:40 -0300

    chore: Adiciona o hook black ao pre-commit

--- src/infra/db/settings/connection_test.py
+++ src/infra/db/settings/connection_test.py
@@ -1,6 +1,7 @@
 import pytest
 from .connection import DBConnectionHandler
 
+
 @pytest.mark.skip(reason="Sensive test")
 def test_create_database_engine():
     db_connection_handle = DBConnectionHandler()

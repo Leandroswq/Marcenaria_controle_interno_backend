33ec12a4fb9b821036110dcf1554b734f06e4c99
Leandroswq <leandrobonfim15@hotmail.com>
2023-07-28 12:20:20 -0300

    feat: Testa a geração do path string do sqlalquemy

--- utils/db.py
+++ utils/db.py
@@ -5,20 +5,20 @@ load_dotenv()
 
 db_envs = {
     "type": os.getenv("DB_TYPE"),
+    "name": os.getenv("DB_NAME"),
     "host": os.getenv("DB_HOST"),
     "user": os.getenv("DB_USER"),
     "password": os.getenv("DB_PASSWORD"),
-    "name": os.getenv("DB_NAME"),
     "show_logs": True if os.getenv("DB_SHOW_LOGS") == "True" else False,
 }
 
 
 def create_sqlalchemy_connection_string(
     type=db_envs["type"],
+    name=db_envs["name"],
     host=db_envs["host"],
     user=db_envs["user"],
     password=db_envs["password"],
-    name=db_envs["name"],
 ):
     if type == "sqlite":
         # Gera a string de conexão para o SQLite

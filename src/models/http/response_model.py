e484836f2056af0f88a0b7888962bc8f595266c8
Leandroswq <leandrobonfim15@hotmail.com>
2023-08-01 21:15:34 -0300

    feat: Criação dos models padrões da resposta http
    gerador de resposta com body, sem body e respostas para casos de sucesso de delete, insert e update

---
+++ src/models/http/response_model.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+def no_body_response_model_factory(response_message: str):
+    class NoBodyResponseModel(BaseModel):
+        detail: str = response_message
+
+    return NoBodyResponseModel
+
+
+def response_model_factory(
+    response_data: object,
+    response_message: str = "Operação efetuada com sucesso ",
+) -> object:
+    class NoBodyResponseModel(BaseModel):
+        detail: Optional[str] = response_message
+        data: response_data
+
+    return NoBodyResponseModel
+
+
+Delete_response_model = no_body_response_model_factory("Excluido com sucesso")
+Insert_response_model = no_body_response_model_factory("Inserido com sucesso")
+Update_response_model = no_body_response_model_factory("Atualizado com sucesso")
